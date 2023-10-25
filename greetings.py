c1d73983175c33be3dafbea4240098e211edc03f
jayjgurung <newuser@Jxxded-MacBook-Pro.local>
2023-10-25 19:12:04 -0400

    +greeting +pluralize

---
+++ greetings.py
@@ -0,0 +1,5 @@
+def greet():
+    print("Hello\n")
+
+for i in range(4):
+    greet()
---
+++ pluralize.py
@@ -0,0 +1,5 @@
+def pluralize(word):
+    return(word+"s")
+
+userInput = input("Word Please..\n: ")
+print(pluralize(userInput))
